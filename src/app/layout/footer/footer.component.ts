5d4691ca4bcab503ea389f1831de1aa9adb57516
AndresTS <andres91411@gmail.com>
2025-11-19 13:31:24 -0600

    feat: update footer component with social media links and styles, remove unused spec file

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,8 +8,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
 }
