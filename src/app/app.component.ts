1e92969282392616fbbe5d84c4d471466afba5ad
AndresTS <andres91411@gmail.com>
2025-11-21 14:38:27 -0600

    feat: update favicon path and replace favicon file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.changeFavicon('assets/images/favicon.ico');
+    this.changeFavicon('favicon.ico');
   }
 
   changeFavicon(url: string): void {
